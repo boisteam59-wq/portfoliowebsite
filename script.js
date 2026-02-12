05391e3308a3e3aad87eac071c180ef466e5f73f
Ajay Kymar <boisteam59@gmail.com>
2026-02-12 11:57:04 +0530

    updated backend to connect render

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ form.addEventListener("submit", async (e) => {
   const message = document.getElementById("message").value;
 
   try {
-    const response = await fetch("http://localhost:5000/api/contact", {
+    const response = await fetch("https://mywebsite-1pm6.onrender.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
